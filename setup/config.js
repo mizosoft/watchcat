5c7e7b01bf8a8e6775e29eb6bc2dac877c6d5995
Moataz Abdelnasser <moataz.nasser20@gmail.com>
2023-03-21 03:05:36 +0200

    Docker config

--- setup/config.js
+++ setup/config.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 export function fromEnv() {
   return {
-    port: process.env.PORT || 45678,
+    port: process.env.APP_PORT || 8080,
     jwtSecret: process.env.JWT_SECRET || 'vErY sEcURe',
-    mongodbUrl: process.env.MONGODB_URL,
+    mongodbUrl: `mongodb://${process.env.MONGODB_HOST || 'localhost'}:${process.env.MONGODB_PORT || 27017}`,
     mongooseOptions: {}
   }
 }
